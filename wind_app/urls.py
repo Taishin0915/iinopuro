53023ed7f9fd223531eb0c35c0605a6849fd0fbd
渡邉大心 <watanabedaishin@watanabedaishinnoMacBook-Pro.local>
2025-08-24 20:22:37 +0900

    Initial commit

---
+++ wind_app/urls.py
@@ -0,0 +1,9 @@
+# myapp/urls.py (新規作成)
+
+# wind_app/urls.py
+from django.urls import path
+from . import views # ← viewsを正しくインポートしているか
+
+urlpatterns = [
+    path('', views.index, name='index'), # ← pathの第一引数は空文字''
+]
