f7397cd959c9c406560a85b9d4827d610761d5ee
渡邉大心 <watanabedaishin@watanabedaishinnoMacBook-Pro.local>
2025-10-07 14:10:18 +0900

    deploy hook test again

--- config/settings.py
+++ config/settings.py
@@ -150,6 +150,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # アップロードされたファイルにアクセスするためのURL
 MEDIA_URL = '/media/'
 
+# ファイルアップロードサイズの設定
+# 最大アップロードサイズを10MBに設定
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB
+
 
 
 
